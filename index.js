2cf5d5ea9ec213a8655ac37d4e2ae3445bc08e71
Wesley Chen <weschen1996@gmail.com>
2020-03-09 18:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Code your solution here
 
 findMatching = (drivers, string) => {
-  
-}
+  drivers.filter( driver => driver.toUpperCase() === string.downcase)
+};
 
 
 fuzzyMatch = (drivers, string) => {
   
-}
+};
+
+matchName = (drivers, string) => {
+  
+};
